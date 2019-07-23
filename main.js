e69276493e30ce5f11b6a6f1c0c3334679bceb19
wiem-sayari23 <52937703+wiem-sayari23@users.noreply.github.com>
2019-07-23 13:47:50 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+var getid=document.getElementById("paragraph") ;
+function A (){
+    getid.style.fontWeight="bold";
+}
+
+console.log("hello")
+var getid=document.getElementById("paragraph");
+function x () {
+    getid.style.fontStyle="italic";
+}
+console.log("hello") 
+var getid=document.getElementById("paragraph");
+function d (){
+    getid.style.textDecoration="underline";
+}
+function fontSize() {
+    getid.style.fontSize=document.getElementById("sz").value;
+}
+function police (ll) {
+    getid.style.fontFamily= ll;
+}
+
+
+
+    
